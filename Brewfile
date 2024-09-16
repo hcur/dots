b7c63513288ad07771c75915c6f81cfe87439f11
Hayden Curfman <hcurfman@keemail.me>
2024-09-16 09:32:11 -0400

    push changes

--- Brewfile
+++ Brewfile
@@ -3,8 +3,15 @@ brew 'gh'
 brew 'stow'
 brew 'neovim'
 brew 'tmux'
-brew 'macchina'
 
+brew 'macchina' # neofetch replacement
+brew 'lsd'      #       ls replacement
+
+
+brew 'jdtls'
+
+cask 'arc'
 cask 'steam'
 cask 'transmission'
 cask 'iterm2'
+cask 'topnotch'
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,36 @@
+-- pckr setup & package installation
+local function bootstrap_pckr()
+  local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
+
+  if not vim.uv.fs_stat(pckr_path) then
+    vim.fn.system({
+      'git',
+      'clone',
+      "--filter=blob:none",
+      'https://github.com/lewis6991/pckr.nvim',
+      pckr_path
+    })
+  end
+
+  vim.opt.rtp:prepend(pckr_path)
+end
+
+bootstrap_pckr()
+
+require('pckr').add{
+
+        -- themes
+        'bluz71/vim-moonfly-colors';
+        'tiagovla/tokyodark.nvim';
+        'EdenEast/nightfox.nvim';
+
+        -- lsp
+        'neovim/nvim-lspconfig';
+        'hrsh7th/nvim-cmp';
+        'hrsh7th/cmp-nvim-lsp';
+
+}
+
 -- general settings
 vim.cmd [[
   set encoding=utf-8
@@ -27,27 +60,42 @@ vim.cmd [[
 
   nnoremap j gj
   nnoremap k gk
+
+  colorscheme moonfly
 ]]
 
 
--- pckr setup & package installation
-local function bootstrap_pckr()
-  local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
+-- nvim-cmp setup
+local cmp = require 'cmp'
+local capabilities = require 'cmp_nvim_lsp'.default_capabilities()
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.snippet.expand(args.body)
+    end,
+  },
 
-  if not vim.uv.fs_stat(pckr_path) then
-    vim.fn.system({
-      'git',
-      'clone',
-      "--filter=blob:none",
-      'https://github.com/lewis6991/pckr.nvim',
-      pckr_path
-    })
-  end
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
 
-  vim.opt.rtp:prepend(pckr_path)
-end
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
 
-bootstrap_pckr()
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+  }, {
+    { name = 'buffer' },
+  })
+})
 
-require('pckr').add{
+-- lsp config
+require'lspconfig'.jdtls.setup{
+  capabilities = capabilities
 }
